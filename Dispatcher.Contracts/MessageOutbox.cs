52bc2b2d7149246b828e3a7cfa1ded0f46269fca
grd.mario <grd.mario@gmail.com>
2023-10-11 13:32:47 +0200

    First Commit

---
+++ Dispatcher.Contracts/MessageOutbox.cs
@@ -0,0 +1,29 @@
+﻿namespace Dispatcher.Contracts
+{
+    using System;
+
+    public class MessageOutbox
+    {
+        public int Id { get; set; }
+
+        public Guid? ConversationId { get; set; }
+
+        public Guid? CorrelationId { get; set; }
+
+        public Guid MessageId { get; set; }
+
+        public string PayloadType { get; set; } = default!;
+
+        public short PayloadVersion { get; set; }
+
+        public string Payload { get; set; } = default!;
+
+        public string Destination { get; set; } = default!;
+
+        public int Status { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public DateTimeOffset? ProcessedAt { get; set; }
+    }
+}
